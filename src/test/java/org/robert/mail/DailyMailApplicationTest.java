b46e9caef2a214310668934e0a2836934902d1cf
robert.lee <robert0714@gmail.com>
2018-03-04 17:42:13 +0800

    test

---
+++ src/test/java/org/robert/mail/DailyMailApplicationTest.java
@@ -0,0 +1,36 @@
+package org.robert.mail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ReflectionUtils;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailyMailApplicationTest {
+	
+	@Autowired
+	private DailyMailApplication application;
+
+	@Before
+	public void setUp() throws Exception {
+//		ReflectionUtils.setField(DailyMailApplication.class.getDeclaredField("folder"), application, "/home/robert0714/Desktop/ec.txt");
+		//-Dorg.robert.mail.attachment=/home/robert0714/Desktop/ec.txt
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRun() throws Exception {
+		
+		
+//		application.run(null);
+		
+	}
+
+}
